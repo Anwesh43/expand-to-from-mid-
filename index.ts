ac29834f154839decfce0bb96551387cd4568982
Anwesh43 <anweshthecool0@gmail.com>
2020-08-29 18:22:18 +0530

    adding ExpandToFromMid class

--- index.ts
+++ index.ts
@@ -181,3 +181,26 @@ class ETFMNode {
         return this 
     }
 }
+
+class ExpanToFromMid {
+
+    curr : ETFMNode = new ETFMNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
