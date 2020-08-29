426aa8a11fe6def4636b3bb6556851eb0210706f
Anwesh43 <anweshthecool0@gmail.com>
2020-08-29 18:17:13 +0530

    adding ETFMNode class

--- index.ts
+++ index.ts
@@ -140,3 +140,44 @@ class Animator {
         }
     }
 }
+
+class ETFMNode {
+
+    next : ETFMNode 
+    prev : ETFMNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ETFMNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawETFMNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+    
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : ETFMNode {
+        var curr : ETFMNode = new ETFMNode(this.i + 1)
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
